0f226650961b3bebf525c1b05dde51fdddc46556
pelicanpubis <norah.binsalamah@hotmail.com>
2023-12-06 18:36:51 +0100

    planet model kommentar

--- GreenThumbVg/Models/PlantModel.cs
+++ GreenThumbVg/Models/PlantModel.cs
@@ -14,22 +14,33 @@ namespace GreenThumbVg.Models
 
         public string NameOfPlant { get; set; } = null!;
 
-       public List<InstructionModel> Instructions { get; set; } = new();
+        // Lista av 'InstructionModel' för växtens skötselinstruktioner
+        public List<InstructionModel> Instructions { get; set; } = new();
 
+        // Lista av 'GardenPlant' för olika trädgårdsinstallationer av växten
         public List<GardenPlant> GardenPlants { get; set; } = new();
-
-
-
 
 
         public PlantModel()
         {
             
         }
+
+        // Konstruktor för 'PlantModel' som tar emot ett växtnamn
+
         public PlantModel(string nameOfPlant)
         {
             NameOfPlant = nameOfPlant;
+
         }
 
+
+        public PlantModel(string nameOfPlant, int plantId, List<InstructionModel> instructions, List<GardenPlant> gardenPlants)
+        {
+            NameOfPlant = nameOfPlant; // Tilldelar det inkommande namnet till 'NameOfPlant'
+            PlantId = plantId; // Tilldelar det inkommande ID:t till 'PlantId'
+            Instructions = instructions; // Tilldelar den inkommande listan av instruktioner till 'Instructions'
+            GardenPlants = gardenPlants; // Tilldelar den inkommande listan av trädgårdsinstallationer till 'GardenPlants'
+        }
     }
 }
