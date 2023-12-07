4ddd0a4f47d14af997aaf1c70d904aede448a743
pelicanpubis <norah.binsalamah@hotmail.com>
2023-12-07 19:22:10 +0100

    garden

---
+++ GreenThumbVg/Migrations/20231207181947_usersGarden.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenThumbVg.Migrations
+{
+    /// <inheritdoc />
+    public partial class usersGarden : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GardenId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GardenId",
+                table: "Users");
+        }
+    }
+}
